99dbb0b093c3b73327f812b3ce4bc1603a45e476
JoseskVolpe <joseskvolpe@gmail.com>
2021-07-18 17:43:24 -0300

    Update Paragraph.java
    NPE fix

--- src/pixelyvulpine/api/lcdui/Paragraph.java
+++ src/pixelyvulpine/api/lcdui/Paragraph.java
@@ -152,7 +152,8 @@ public class Paragraph {
 	
 	public void setText(String text) {
 		
-		if(this.text.equals(text)) return;
+		if(this.text==null || this.text.equals(text)) return;
+		
 		
 		this.text=text;
 		prepareDimension(width, height);
